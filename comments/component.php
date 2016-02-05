8b3fa25f30938277f43c5cb3d77283d4a254d787
a.solovyov <atom1c1155@gmail.com>
2016-02-05 22:25:41 +0300

    мелкие правки

--- comments/component.php
+++ comments/component.php
@@ -10,36 +10,41 @@ use Bitrix\Highloadblock as HL;
 use Bitrix\Main\Entity;
 use Bitrix\Main\Type\DateTime;
 use Bitrix\Main\Page\Asset;
+use \Bitrix\Main\Loader;
+use \Bitrix\Main\Application;
 
 // Include Bootstrap and jQuery
 Asset::getInstance()->addJs('http://code.jquery.com/jquery-2.1.4.min.js');
 Asset::getInstance()->addJs('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js');
 $APPLICATION->SetAdditionalCSS('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css');
 
-CModule::IncludeModule("highloadblock");
+\Bitrix\Main\Loader::IncludeModule("iblock");
 
-$user_name = $_POST['user_name'];
-$message = $_POST['message'];
-$submit = $_POST['submit'];
+$context = Application::getInstance()->getContext();
+$request = $context->getRequest();
+
+$user_name = $request->get("user_name");
+$message = $request->get("message");
+$submit = $request->get("submit");
 $highloadID = $arParams['HIGHLOAD_BLOCK_ID'];
 
 
-if ($USER->IsAuthorized())
+if ($USER->IsAuthorized()) {
     $un = $USER->GetFullName();
-else
+} else {
     $un = $user_name;
-
-// Element or page type
-if (!empty($arParams['OBJECT_COMMENT_TYPE']))
-{
-    if ($arParams['OBJECT_COMMENT_TYPE'] == 'iblock_id')
-        $elementID = $arParams['OBJECT_COMMENT_STRING'];
-    elseif ($arParams['OBJECT_COMMENT_TYPE'] == 'page')
-        $pageID = $arParams['OBJECT_COMMENT_STRING'];
 }
 
-if (!empty($submit) && !empty($message) && !empty($un))
-{
+// Element or page type
+if (!empty($arParams['OBJECT_COMMENT_TYPE'])) {
+    if ($arParams['OBJECT_COMMENT_TYPE'] == 'iblock_id') {
+        $elementID = $arParams['OBJECT_COMMENT_STRING'];
+    } elseif ($arParams['OBJECT_COMMENT_TYPE'] == 'page') {
+        $pageID = $arParams['OBJECT_COMMENT_STRING'];  
+    } 
+}
+
+if (!empty($submit) && !empty($message) && !empty($un)) {
     $hlblock = HL\HighloadBlockTable::getById($highloadID)->fetch();
     $entity = HL\HighloadBlockTable::compileEntity($hlblock);
     $entity_data_class = $entity->getDataClass();
@@ -52,11 +57,11 @@ if (!empty($submit) && !empty($message) && !empty($un))
         'UF_PAGE_ID' => $pageID
     );
 
-    if ($resAdd = $entity_data_class::add($arData))
-    {
+    if ($resAdd = $entity_data_class::add($arData)) {
         $mess = 'Спасибо! Ваше сообщение отправлено.';
         BXClearCache(true, "/".SITE_ID.$this->GetRelativePath());
     }
+
     $comData = $entity_data_class::getList(array(
         "select" => array("*"),
         "order" => array("ID" => "DESC"),
@@ -81,8 +86,7 @@ if (!empty($submit) && !empty($message) && !empty($un))
     echo json_encode($resData);
     exit();
 }
-if ($this->StartResultCache(3600, $pageID.$elementID.$USER->GetGroups(), "/".SITE_ID.$this->GetRelativePath()))
-{
+if ($this->StartResultCache(3600, $pageID.$elementID.$USER->GetGroups(), "/".SITE_ID.$this->GetRelativePath())) {
     $hlblock = HL\HighloadBlockTable::getById($highloadID)->fetch();
     $entity = HL\HighloadBlockTable::compileEntity($hlblock);
     $entity_data_class = $entity->getDataClass();
@@ -94,8 +98,7 @@ if ($this->StartResultCache(3600, $pageID.$elementID.$USER->GetGroups(), "/".SIT
             'UF_PAGE_ID' => $pageID
         )
     ));
-    while ($arComData = $comData->Fetch())
-    {
+    while ($arComData = $comData->Fetch()) {
         $arResult['COMMENTS_ITEMS'][] = $arComData;
     }
 
